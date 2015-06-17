872152ec2b692c2c3afe8f8f737033e0c60afcb2
adamzuckerberg <adam@adamzuckerberg.com>
2015-06-17 09:05:41 -0700

    Remove personalize button. Update require line in application js for touchpunch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require jquery.ui.touch-punch.min
+//= require jquery.ui.touch-punch
 //= require foundation
 //= require turbolinks
 //= require_tree .
